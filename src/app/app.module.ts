5a30110a986a1ef23587f9625a3a524591cae24c
MrRobotProgrammer <flavio.santos@pravaler.com.br>
2020-10-16 21:13:33 -0300

    Criando um Core Module Com Servicos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,16 +11,14 @@ import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { RestaurantComponent } from './restaurants/restaurant/restaurant.component';
-import { RestaurantsService } from './restaurants/restaurants.service';
 import { RestaurantDatailComponent } from './restaurant-datail/restaurant-datail.component';
 import { MenuComponent } from './restaurant-datail/menu/menu.component';
 import { ShoppingCartComponent } from './restaurant-datail/shopping-cart/shopping-cart.component';
 import { MenuItemComponent } from './restaurant-datail/menu-item/menu-item.component';
 import { ReviewsComponent } from './restaurant-datail/reviews/reviews.component'
-import { ShoppingCartService } from './restaurant-datail/shopping-cart/shopping-cart.service';
-import { OrderService } from './order/order.service';
 import { OrderSummaryComponent } from './order-summary/order-summary.component';
 import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core/core.module';
 
 
 @NgModule({
@@ -41,9 +39,10 @@ import { SharedModule } from './shared/shared.module';
     BrowserModule,
     HttpModule,
     SharedModule,
+    CoreModule,
     RouterModule.forRoot(ROUTES)
   ],
-  providers: [RestaurantsService, ShoppingCartService, OrderService, {provide: LOCALE_ID, useValue: 'pt-BR'}],
+  providers: [{provide: LOCALE_ID, useValue: 'pt-BR'}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
